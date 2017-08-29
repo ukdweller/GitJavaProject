96035c1ab20fa1a738d484aae7c321a59f06ba3b
ukdweller <glesgadweller@gmail.com>
2017-08-28 22:29:23 -0400

    added another line

--- src/test/App.java
+++ src/test/App.java
@@ -1,9 +1,10 @@
 package test;
 
 public class App {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello from Java");
+		System.out.println("Hello from Java");
 
 	}
 
